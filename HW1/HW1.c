f37052e1bc1da9c107dc8a73d840b8c84b231604
Romeo P <romeperl@umd.edu>
2026-02-18 13:09:49 -0500

    balling!

--- HW1/HW1.c
+++ HW1/HW1.c
@@ -5,102 +5,110 @@
 int main(int argc, char** argv)
 {
     // Declare the name of the file to transport data
-    char filename[] = "transporter";
+    char filename[] = "./transporter";
 
     // Make the first pid number collector
     pid_t child1_PID;
-    pid_t child2_PID;
+    pid_t child2_PID = -1; // this is a signed integer
+
+    printf("\n\t\tWelcome to romeo's addR!\n");
 
     // Fork one child
     child1_PID = fork();
+    // Fork the next child
+    if(child1_PID > 0)
+    {
+        // Only let the parent fork
+        child2_PID = fork();
+    }
 
     // find the child and parent, and make the parent fork one more time
     // This first child will be the one to create the file and use user inputs
-    if(child1_PID == 0)
+    if(child1_PID == 0 && child2_PID == -1)
     {
         // Make the file to read/write from
         // To be safe, we'll make two files - one for signalling that we've received an input, and one for the input
-        FILE* signal_hermano = fopen("./signal", "w+");
-        FILE* send_inputs = fopen("./inputs", "w+");
+        FILE* transport_data = fopen(filename, "w+");
 
-        // close the files immediate now that they are created
-        fclose(signal_hermano);
-        fclose(send_inputs);
+        // close the file immediately now that they are created
+        fclose(transport_data);
 
         while(1)
         {   
-            
-            printf("\nWelcome to romeo's addR!\n");
-
             // number inputs
             float num1, num2;
             int rc;
 
-            printf("\tInput the first number you'd like you'd like to add: \n\t");
+            // open da files back up to write to them
+            transport_data = fopen(filename, "w");
+            fprintf(transport_data,"0\n"); // reset the signal
+            fclose(transport_data);
+
+            printf("\n[child 1: receiver] Input the 1st number you'd like you'd like to add: ");
             scanf("%f", &num1);
 
-            // open da files back up to write to them
-            signal_hermano = fopen("./signal", "w");
-            fprintf(signal_hermano,"0"); // reset the signal
-            fclose(signal_hermano);
-
-            printf("\tInput the second number you'd like you'd like to add: \n\t");
+            printf("[child 1: receiver] Input the 2nd number you'd like you'd like to add: ");
             scanf("%f", &num2);
 
-            printf("\tadding the following numbers: \n\t\t%f, %f", num1, num2);
-            send_inputs = fopen("./inputs", "w");
-            fprintf(send_inputs, "%f\n%f", num1, num2);
-            fclose(send_inputs);
-
-            printf("\n\tnumber sent off to adder sibling!\n\n");
+            printf("[child 1: reciever] Adding the following numbers: %f, %f\n", num1, num2);
 
             // Inform our sibling that we are ready to read
-            signal_hermano = fopen("./signal", "w");
-            fprintf(signal_hermano,"1"); // reset the signal
-            fclose(signal_hermano);
-
+            transport_data = fopen(filename, "w");
+            fprintf(transport_data,"1\n%f\n%f", num1, num2); // reset the signal
+            fclose(transport_data);
+            
         }
 
-        // if the parent tells us to kill ourselves, we follow their orders
-        // now remove them suckas
-        remove("./signal");
-        remove("./inputs");
-
-        // and finally, exit
         return 1;
 
     }
-    else if(child1_PID > 0)
+    // The second child
+    else if(child2_PID == 0)
     {
-        // Fork one more time
-        child2_PID = fork(); // reuse this variable as the child process will never have acces to it, unless.....
+        // read
+        FILE* transporter = fopen(filename, "r+");
 
-        // This child will read in inputs from the file and add two ints
-        if(child2_PID == 0) 
-        {
-            // read
-            FILE* hermano_signal = fopen("./signal", "r+");
-            FILE* inputs_sent = fopen("./inputs", "r+");
+        // close the files immediate now that they are created
+        fclose(transporter);
 
-            int signal, rc;
-            while(1)
-                rc = fscanf(hermano_signal, "%d", &signal);
-                if(rc != EOF)
-                {
-                    if(signal == 1)
-                    printf("\n\t\t\tchild 2 - got inputs!\n");
-                }
-        }
-        else if(child2_PID > 0)
+        int signal, reading, rc;
+        float num1, num2, final;
+        while(1)
         {
-            // The parent will just monitor the children playing, and tell them them it's bedtime when we ctrl-c the process. As in the parent will KILL the children. thats right. you heard me.
-            // Monitor for ctrl-c, somehow?
-            while(1)
+            transporter = fopen(filename, "r");
+
+            rc = fscanf(transporter, "%d", &signal);
+            if(rc != EOF)
             {
-
+                if(signal == 1 && reading != 1)
+                {
+                    reading = 1;
+                    fscanf(transporter, "%f", &num1);
+                    fscanf(transporter, "%f", &num2);
+                    final = num1+num2;
+                    printf("[child 2: responder] Sum of %f and %f: \t%f\n", num1, num2, final);
+                }
+                else if(signal == 0 && reading == 1)
+                {
+                    reading = 0;
+                }
             }
+            // close the files immediate now that they are created
+            fclose(transporter);
+        }
+
+    }
+    // The parent
+    else if(child2_PID > 0 && child1_PID > 0)
+    {
+        // The parent will just monitor the children playing, and tell them them it's bedtime when we ctrl-c the process. As in the parent will KILL the children. thats right. you heard me.
+        // Monitor for ctrl-c, somehow?
+
+        while(1)
+        {
 
         }
+
     }
 
 }
